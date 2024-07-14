527cbb3715509d2d69732c319904b066fbdf2d16
AshPanda <alena.shwaika@gmail.com>
2024-07-14 03:48:57 +0300

    Commit Task1,2,3,4

--- Task1.java
+++ Task1.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
         int idealFenceLength = 346;
-        System.out.println("Введите длину забора сантиметрах:");
+        System.out.println("Введите длину забора в сантиметрах:");
         Scanner userInput = new Scanner(System.in);
         final String input = userInput.nextLine();
         int userFenceLength;
